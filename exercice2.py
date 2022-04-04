7248f1edd4fbe6b6631a87484d022256af05511a
kowaqs <alex.caron.simplon@gmail.com>
2022-04-04 13:55:02 +0200

    exo2 en python

---
+++ exercice2.py
@@ -0,0 +1,14 @@
+i=0
+j=0
+nombreColone = int(input("Nombre de colonnes?\n"))
+nombreLigne= int(input("Nombre de lignes ?\n"))
+
+while j <nombreLigne :
+	while i < nombreColone :
+		print("@",end ='')
+		i = i + 1
+	print("")
+
+	i = 0 
+
+	j = j +1
